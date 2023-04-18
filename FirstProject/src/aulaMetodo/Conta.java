bf7b0c75ebfb83f35e327dbe2f1780e028d1e7dc
Sofia Nascimento <sofia.n06@aluno.ifsc.edu.br>
2023-04-18 19:43:41 -0300

    Primeiras aulas

---
+++ FirstProject/src/aulaMetodo/Conta.java
@@ -0,0 +1,45 @@
+package aulaMetodo;
+
+public class Conta {
+	private String name;
+	private double saldo;
+
+	public Conta(String nomeInicial, double saldoInicial){
+		name = nomeInicial;
+
+		if (saldoInicial > 0){
+			saldo = saldoInicial;
+		}
+		else {
+			saldo = 0;
+		}
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void saque(double valorSaque){
+		if (valorSaque <= saldo) {
+			this.saldo = saldo - valorSaque;
+		}
+		else {
+			System.out.print("Valor do saque excede o saldo da sua conta! Operação cancelada!\n");
+		}
+	}
+
+	public void deposito(double valorDeposito){
+		if (valorDeposito > 0){
+			saldo = saldo + valorDeposito;
+		}
+
+	}
+
+	public double getSaldo(){
+		return saldo;
+	}
+}
---
+++ FirstProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Sofia
+ *
+ */
+module FirstProject {
+}
